3fa035f273ca42304b632de8fff6db740df4ff9a
floryw <batman2796rus@gmail.com>
2026-02-24 16:41:57 +0300

    База сайта

--- main/app/models.py
+++ main/app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ProfileUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fio = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+
+
+class MasterClass(models.Model):
+
+    TYPE_CHOICES = (
+        (1, "Онлайн"),
+        (2, "Очная встреча"),
+    )
+
+    PAYTYPE_CHOICES = (
+        (1, "банковская карта"),
+        (2, "онлайн-перевод"),
+    )
+
+    STATUS_CHOICES = (
+        (1, "банковская карта"), # Скорее всего здесь опечатка (скопировано с PAYTYPE), но пока оставляю так же
+        (2, "онлайн-перевод"),
+    )
+
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, verbose_name='Название курса')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    description = models.CharField(max_length=200, verbose_name='Описание')
+    count = models.CharField(max_length=200)
+    date_event = models.DateField(verbose_name='Дата события')
+    type_event = models.IntegerField(choices=TYPE_CHOICES, verbose_name='Формат')
+    time_event = models.CharField(max_length=200, verbose_name='Длительность')
+    price = models.CharField(max_length=200, verbose_name='Цена')
+    paytype = models.IntegerField(choices=PAYTYPE_CHOICES, verbose_name='Способ оплаты')
+    status = models.IntegerField(choices=STATUS_CHOICES, verbose_name='Статус')
+
+
+
--- main/main/urls.py
+++ main/main/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import index, logout_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('logout/', logout_view, name='logout'),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name='login'),
 ]
